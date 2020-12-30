fba4007436f9d89cccd32f974cca45720be04362
mikiasgv <mikiasgv@gmail.com>
2020-12-30 09:55:51 -0500

    story content complete

--- src/components/StoryComponent.vue
+++ src/components/StoryComponent.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="story__pictures">Story</div>
+  <div class="story__pictures">
+    <img
+      src="img/story-1.jpeg"
+      alt="Couple with a new home"
+      class="story__img--1"
+    />
+    <img src="img/story-2.jpeg" alt="New house" class="story__img--2" />
+  </div>
 </template>
 
 <script>
