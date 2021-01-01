d19aab76d2565e301e8b2702c4503e0f80869e64
mikigv <mikiasgv@gmail.com>
2021-01-01 22:56:31 +0300

    Implement the header section

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,17 @@
 <template>
-  <header class="header">Header</header>
+  <header class="header">
+    <img src="img/logo.png" alt="Nexter logo" class="header__logo" />
+    <h3 class="heading-3">Your own home:</h3>
+    <h1 class="heading-1">The ultimate personal freedom</h1>
+    <button class="btn header__btn">View our properties</button>
+    <div class="header__seenon-text">Seen on</div>
+    <div class="header__seenon-logos">
+      <img src="img/logo-bbc.png" alt="Seen on logo 1" />
+      <img src="img/logo-forbes.png" alt="Seen on logo 2" />
+      <img src="img/logo-techcrunch.png" alt="Seen on logo 3" />
+      <img src="img/logo-bi.png" alt="Seen on logo 4" />
+    </div>
+  </header>
 </template>
 
 <script>
@@ -7,5 +19,4 @@ export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
